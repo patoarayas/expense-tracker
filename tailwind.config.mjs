9d8246245eaced06d659f3fbf72f75dc4e6e9770
patoarayas <pato.araya.gonzalez@gmail.com>
2024-01-10 23:07:01 -0300

    config project

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  plugins: [nextui()],
+};
